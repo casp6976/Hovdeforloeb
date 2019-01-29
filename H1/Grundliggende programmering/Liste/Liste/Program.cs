ad8bdc16f025e42ce1bfd5aefcc18928563cc201
Casper Ryttergaard Vindum <casp6976@zbc.dk>
2019-01-29 09:02:59 +0100

    added comments

--- H1/Grundliggende programmering/Liste/Liste/Program.cs
+++ H1/Grundliggende programmering/Liste/Liste/Program.cs
@@ -10,13 +10,49 @@ namespace Liste
     {
         static void Main(string[] args)
         {
+            //starting out by making a list with all the even numbers from 1 to 20
             List<int> listeB = new List<int> { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
 
-            for (int i = 0; i <= listeB.; i++)
+            //shows the list to the user
+            for (int i = 0; i < listeB.Count; i++)
             {
-                   //// jahah /// 
-                   ///
+                Console.WriteLine(listeB[i]);
             }
+            Console.ReadKey();
+
+            //removing the numbers 6, 12 and 18
+            listeB.Remove(6);
+            listeB.Remove(12);
+            listeB.Remove(18);
+
+            //shows the updated list to the user
+            for (int i = 0; i < listeB.Count; i++)
+            {
+                Console.WriteLine(listeB[i]);
+            }
+            Console.ReadKey();
+
+            //insets number 17 in the 3rd index space
+            listeB.Insert(2, 17);
+
+            //shows the updated list to the user
+            for (int i = 0; i < listeB.Count; i++)
+            {
+                Console.WriteLine(listeB[i]);
+            }
+            Console.ReadKey();
+
+            //creates a new list containing the values from the old
+            List<int> listeC = listeB;
+            //reverses the order of the numbers in the new list
+            listeC.Reverse();
+
+            //shows the new list to the user
+            for (int i = 0; i < listeB.Count; i++)
+            {
+                Console.WriteLine(listeC[i]);
+            }
+            Console.ReadKey();
         }
     }
 }
