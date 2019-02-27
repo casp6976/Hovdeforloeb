8cccd8a9ce8e4ca22079f9e74f552ca96326f234
Casper Ryttergaard Vindum <casp6976@zbc.dk>
2019-02-27 12:42:56 +0100

    bob

---
+++ H1/Grundliggende programmering/Mozart/Mozart/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+
+namespace Mozart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+            Random randomNode = new Random();
+            SoundPlayer player = new SoundPlayer();
+            List<string> Minuetten = new List<string>();
+            List<string> Trioen = new List<string>();
+            for (int i = 1; i < 177; i++)
+            {
+                Minuetten.Add("M" + i);
+            }
+            for (int i = 1; i < 97; i++)
+            {
+                Trioen.Add("T" + i);
+            }
+
+            while (true)
+            {
+                player.SoundLocation = AppDomain.CurrentDomain.BaseDirectory + $"{Minuetten[randomNode.Next(0, Minuetten.Count)]}.wav";
+                player.PlaySync();
+                player.SoundLocation = AppDomain.CurrentDomain.BaseDirectory + $"{Trioen[randomNode.Next(0, Trioen.Count)]}.wav";
+                player.PlaySync();
+            }
+
+
+
+
+        }
+    }
+}
