7e15b23f007082711b8c4d79b4ffa5ad729c5884
Casper Ryttergaard Vindum <casp6976@zbc.dk>
2019-03-14 12:33:40 +0100

    completed task A, unsure about task B

---
+++ H1/Objekt orienteret programmering/Geometri/Geometri/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square firstSquare = new Square(5);
+            Square secondSquare = new Square(18);
+            Square thirdSquare = new Square(11);
+
+
+            Console.WriteLine(firstSquare.Perimeter());
+            Console.WriteLine(secondSquare.Perimeter());
+            Console.WriteLine(thirdSquare.Perimeter());
+            Console.WriteLine(firstSquare.Area());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ H1/Objekt orienteret programmering/Geometri/Geometri/Square.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometri
+{
+    public class Square
+    {
+        private int pSide;
+
+        public int Side
+        {
+            get
+            {
+                return pSide;
+            }
+            set
+            {
+                pSide = value;
+            }
+        }
+
+        /// <summary>
+        /// constructor for squares
+        /// </summary>
+        /// <param name="squareSide"></param>
+        public Square(int squareSide)
+        {
+            Side = squareSide;
+        }
+
+        /// <summary>
+        /// returns target perimeter
+        /// </summary>
+        /// <returns></returns>
+        public int Perimeter()
+        {
+            int perimeter = 0;
+
+            perimeter = pSide * 4;
+
+            return perimeter;
+        }
+            
+        /// <summary>
+        /// returns target area
+        /// </summary>
+        /// <returns></returns>
+        public int Area()
+        {
+            int area = 0;
+
+            area = pSide * pSide;
+
+            return area;
+        }
+    }
+}
