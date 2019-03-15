8a85c71f42e32d85064270bf35556f85904649ca
Casper Ryttergaard Vindum <casp6976@zbc.dk>
2019-03-15 09:56:02 +0100

    created the initial gui and menu

---
+++ H1/Objekt orienteret programmering/Queue/Queue/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                #region main menu
+                Console.WriteLine(
+                    "Main Menu \n\n" +
+                    "[1] Add patient \n" +
+                    "[2] Call up the next patient \n"
+                    );
+                #endregion
+                string menuInput = Console.ReadLine();
+                switch (menuInput)
+                {
+                    case "1":
+
+                        break;
+
+                    case "2":
+
+                        break;
+                }
+
+
+
+                Console.WriteLine("END OF PROGRAM");
+                Console.ReadLine();
+            }
+        }
+    }
+}
